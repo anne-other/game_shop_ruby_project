4df1dfea6bc1329ea12f8ecdc7989a24a0c8b96b
anne-other <anne.c.ince@gmail.com>
2018-08-12 14:32:07 +0100

    adding save function to product class

--- models/product.rb
+++ models/product.rb
@@ -15,4 +15,21 @@ class Product
     @manufacturer_id = options['manufacturer_id']
   end
 
+  def save()
+    sql = "INSERT INTO products
+    (
+      product_name,
+      description,
+      stock,
+      buying_cost,
+      selling_price,
+      manufacturer_id
+    )
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING id"
+    values = [@product_name, @description, @stock, @buying_cost, @selling_price, @manufacturer_id]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i()
+  end
+
 end
