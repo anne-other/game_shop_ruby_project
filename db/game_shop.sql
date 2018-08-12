a26adbfdc6b6d68a2320076f093c105b18b6457f
anne-other <anne.c.ince@gmail.com>
2018-08-12 11:39:55 +0100

    creating products db table

--- db/game_shop.sql
+++ db/game_shop.sql
@@ -4,10 +4,21 @@ DROP TABLE manufacturers
 CREATE TABLE manufacturers
 (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
+  manufacturer_name VARCHAR(255),
   website VARCHAR(255),
   contact_name VARCHAR(255),
   email VARCHAR(255),
   phone_number VARCHAR(255),
   notes TEXT
+)
+
+CREATE TABLE products
+(
+  id SERIAL PRIMARY KEY,
+  product_name VARCHAR(255),
+  descricption TEXT,
+  stock INT,
+  buying_cost NUMERIC,
+  selling_cost NUMERIC,
+  manufacturer_id INT REFERENCES manufacturers(id) ON DELETE CASCADE
 )
